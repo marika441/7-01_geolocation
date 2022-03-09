f7d666e8521c83ac931bf6cd6e19ace273f323ad
marika441 <92457238+marika441@users.noreply.github.com>
2022-03-10 01:03:02 +0900

    [Add]位置情報の取得

---
+++ script.js
@@ -0,0 +1,11 @@
+'use strict';
+
+function success(pos) {
+  console.log(pos);
+}
+
+function fail(error) {
+  alert('位置情報の取得に失敗しました。エラーコード:' + error.code);
+}
+
+navigator.geolocation.getCurrentPosition(success, fail);
