9472281daeae4bef43f0146cb627be8e6a0d9838
marika441 <92457238+marika441@users.noreply.github.com>
2022-03-10 23:45:34 +0900

    [Finish]7-1位置情報

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 'use strict';
 
 function success(pos) {
-  console.log(pos);
+  // console.log(pos);
+  const lat = pos.coords.latitude;
+  const lng = pos.coords.longitude;
+  const accuracy = pos.coords.accuracy;
+
+  $('#loc').text(`緯度:${lat} 経度:${lng}`);
+  $('#accuracy').text(accuracy);
 }
 
 function fail(error) {
